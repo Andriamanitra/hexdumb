107643135d6e7434762c2d7605564edc1f3aaddc
Andriamanitra <mikko77792@gmail.com>
2023-11-22 12:20:48 +0200

    support cli args

--- hexdumb.cr
+++ hexdumb.cr
@@ -1,4 +1,5 @@
 require "colorize"
+require "option_parser"
 
 module Color
   Ascii        = Colorize::ColorANSI::LightGreen
@@ -8,31 +9,45 @@ module Color
 end
 
 class Hexdumb
-  def initialize(io : IO | String)
-    @contents = io
-    @groupsize = 8
-    @groups_per_line = 2
+  def initialize(
+    @input : IO | String,
+    @max_bytes : UInt32? = nil,
+    @skip = 0_u32,
+    @group_size = 8_u8,
+    @groups_per_line = 2_u8,
+    @bytefmt = "%02x"
+  )
   end
 
   def linesize
-    @groupsize * @groups_per_line
+    @group_size * @groups_per_line
+  end
+
+  def filler
+    nspaces = (@bytefmt % 255).size
+    (" " * nspaces).colorize(:default)
   end
 
   def lines
-    @contents.each_byte.each_slice(linesize)
+    if n = @max_bytes
+      @input.each_byte.skip(@skip).first(n).each_slice(linesize)
+    else
+      @input.each_byte.skip(@skip).each_slice(linesize)
+    end
   end
 
   def grouped(line, element_sep = "", group_sep = " ") : String
-    line.each_slice(@groupsize).map(&.join(element_sep)).join(group_sep)
+    line.each_slice(@group_size).map(&.join(element_sep)).join(group_sep)
   end
 
   def output
     sz = linesize * 3
+
     lines.each_with_index do |bytes, idx|
-      offset = idx * linesize
-      hexbytes = bytes.map { |b| ("%02x" % b).colorize(color(b)) }
+      offset = @skip + idx * linesize
+      hexbytes = bytes.map { |b| (@bytefmt % b).colorize(color(b)) }
       while hexbytes.size < linesize
-        hexbytes.push("  ".colorize(:default))
+        hexbytes.push(filler)
       end
       asciibytes = bytes.map { |b| show_ascii(b.chr).colorize(color(b)) }
       grouped_hex = grouped(hexbytes, " ", "  ")
@@ -70,17 +85,62 @@ class Hexdumb
   end
 end
 
+skip = 0_u32
+max_bytes : UInt32? = nil
+group_size = 8_u8
+groups_per_line = 2_u8
+bytefmt = "%02x"
+
+OptionParser.parse do |parser|
+  parser.banner = "Usage: hexdumb [filename]"
+
+  parser.on("-n N", "--length=N", "Interpret only N bytes of input") do |n|
+    max_bytes = n.to_u32
+  end
+
+  parser.on("-s OFFSET", "--skip=OFFSET", "Skip OFFSET bytes from the beginning of the input") do |n|
+    skip = n.to_u32
+  end
+
+  parser.on("-g N", "--groups=N", "Number of groups per line (default: 2)") do |n|
+    groups_per_line = n.to_u8
+  end
+
+  parser.on("-b N", "--bytes-per-group=N", "Number of bytes per group (default: 8)") do |n|
+    group_size = n.to_u8
+  end
+
+  parser.on("-d", "--decimal", "Show bytes in base 10") do
+    bytefmt = "%3d"
+  end
+
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+end
+
 if STDIN.tty? && ARGV.empty?
   puts "Type something to see it hexdumbed (Ctrl-d to stop)"
   input = STDIN.gets_to_end
   puts
-  Hexdumb.new(input).output
 else
-  begin
-    Hexdumb.new(ARGF).output
-  rescue exc : File::NotFoundError
-    abort "Unable to read file '#{exc.file}': File does not exist"
-  rescue exc : File::AccessDeniedError
-    abort "Unable to read file '#{exc.file}': Access denied"
-  end
+  input = ARGF
+end
+
+hexdumb = Hexdumb.new(
+  input,
+  skip: skip,
+  max_bytes: max_bytes,
+  group_size: group_size,
+  groups_per_line: groups_per_line,
+  bytefmt: bytefmt
+)
+
+begin
+  hexdumb.output
+rescue exc : File::NotFoundError
+  abort "Unable to read file '#{exc.file}': File does not exist"
+rescue exc : File::AccessDeniedError
+  abort "Unable to read file '#{exc.file}': Access denied"
 end
